e542cfd410e23723c2e970c686c4846fe99b0f1d
jay havranek <jay@hvrnk.com>
2015-07-23 20:52:43 -0600

    added image changed name from 'Login' to 'Home'

--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default React.createClass({
-	displayName: 'Login',
+	displayName: 'Home',
 
 	render() {
 		return (
@@ -9,6 +9,7 @@ export default React.createClass({
 				<header role='banner'>
 					<h1><a href='/aboutme'>hvrnk</a></h1>
 				</header>
+				<img src="http://cdn.neonsky.com/4bd5ebf463380/images/_MG_7848_HDR-1.jpg" width="100%" />
 			</div>
 		)
 	}
