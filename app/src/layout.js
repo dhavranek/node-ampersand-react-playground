cc0a1e9abd9f8083a189a38f9caefcb9ccf3aba6
jay havranek <jay@hvrnk.com>
2015-07-23 20:52:12 -0600

    removed nav bar

--- app/src/layout.js
+++ app/src/layout.js
@@ -6,15 +6,6 @@ export default React.createClass({
 	render () {
 		return (
 			<div>
-			  <nav className='top-nav top-nav-light cf' role='navigation'>
-			    <input id='menu-toggle' className='menu-toggle' type='checkbox'/>
-			    <label htmlFor='menu-toggle'>Menu</label>
-			    <ul className='list-unstyled list-inline cf'>
-			      <li><a href='/'>HOME</a></li>
-			      <li><a href='/repos'>RAWR</a></li>
-			      <li className='pull-right'><a href='/logout'>Logout</a></li>
-			    </ul>
-			  </nav>
 			  <div className='container'>
 			  	{this.props.children}
 			  </div>
