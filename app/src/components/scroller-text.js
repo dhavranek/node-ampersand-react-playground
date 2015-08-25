55e9f09a21440d5ebf9e305b91bd22c3d7981350
jay havranek <jay@hvrnk.com>
2015-08-24 19:03:43 -0600

    header starts in center and shrinks to top left corner

--- app/src/components/scroller-text.js
+++ app/src/components/scroller-text.js
@@ -4,12 +4,22 @@ export default React.createClass({
 
   getInitialState () {
     return {
-      fontSize: this.props.maxFontSize
+      fontSize: this.props.maxFontSize,
+      top: 'auto',
+      left: 'auto'
     }
   },
 
   componentDidMount () {
     window.addEventListener( 'scroll', this.updateFontSize );
+    window.addEventListener( 'scroll', this.updateTextPosition );
+
+    var div = this.refs.text.getDOMNode();
+    var height = div.clientHeight;
+    var width = div.clientWidth;
+
+    this.setState({ top: (window.innerHeight - height) / 2});
+    this.setState({ left: (window.innerWidth - width) / 2});
   },
 
   updateFontSize (event) {
@@ -21,13 +31,34 @@ export default React.createClass({
     this.setState({ fontSize: updatedFontSize });
   },
 
+  updateTextPosition (event) {
+    var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+    var text = this.refs.text.getDOMNode();
+
+    var top = (window.innerHeight - text.clientHeight) / 2;
+    var left = (window.innerWidth - text.clientWidth) / 2;
+    left = Math.max(0, left - scrollTop);
+    top = Math.max(scrollTop, top);
+    
+    this.setState({ top: top });
+    this.setState({ left: left });
+  },
+
   render () {
+    var divStyle = {
+      position: 'absolute',
+      top: this.state.top,
+      left: this.state.left
+    };
+
     var logoStyle = {
-      fontSize: this.state.fontSize
+      fontSize: this.state.fontSize,
+      margin: 0
     };
 
     return (
-      <div>
+      <div ref="text" style={divStyle}>
         <h1 style={logoStyle}>{this.props.children}</h1>
       </div>
     )
--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import ScrollerText from '../components/scroller-text'
+import ParallaxImage from '../components/parallax-image'
 
 export default React.createClass({
 	displayName: 'Home',
 
+	void () {
+		<ParallaxImage src="http://cdn.neonsky.com/4bd5ebf463380/images/_MG_7848_HDR-1.jpg" />
+	},
+
 	render() {
+		var div = {
+			height: '100vh',
+			width: '100vw',
+			position: 'relative'
+		};
+
 		return (
 			<div>
-				<header role='banner'>
+				<div style={div}>
 					<ScrollerText maxFontSize={150} minFontSize={50}><a href="/aboutme">hvrnk</a></ScrollerText>
-				</header>
-				<img src="http://cdn.neonsky.com/4bd5ebf463380/images/_MG_7848_HDR-1.jpg" width="100%" />
+				</div>
+				<div style={div}> </div>
 			</div>
 		)
 	}
